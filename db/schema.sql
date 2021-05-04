c56dd3c76d584f05860e11e891cc6b9ccd9e5155
powerar <arpower@bell.net>
2021-05-04 18:01:08 -0400

    updated queries to include party name in get routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -8,5 +11,8 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    -- this tells SQL that you cannot insert a party id if it does not exist in the parties table
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -31,7 +31,11 @@ const db = mysql.createConnection(
 // err returns an error, rows returns a variable with the response from the SQL server
 //
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+               AS party_name
+               FROM candidates
+               LEFT JOIN parties
+               ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -47,7 +51,12 @@ app.get('/api/candidates', (req, res) => {
 
 //get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+               AS party_name 
+               FROM candidates 
+               LEFT JOIN parties 
+               ON candidates.party_id = parties.id 
+               WHERE candidates.id = ?`;
   const params = [req.params.id]; //get the parameters from the request in the URL in the form of an array
 
   db.query(sql, params, (err, row) => {
@@ -92,26 +101,31 @@ app.delete('/api/candidate/:id', (req, res) => {
 // create sql and params variables to pass the query and the query parameters in to the function
 // restructure the req and get the body property
 app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');//imported from a method at the top
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  ); //imported from a method at the top
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
 
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
         VALUES (?, ?, ?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
     res.json({
-        message: 'success',
-        data: body
-        })
+      message: 'success',
+      data: body,
     });
+  });
 });
 
 //handle user requests that are not valid
