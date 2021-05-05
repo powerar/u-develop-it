42e58ad0a546d68d0a813f5dd5ab1152e31be915
powerar <arpower@bell.net>
2021-05-04 20:32:59 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+//import candidateRoutes
 router.use(require('./candidateRoutes'));
 
+//import partyRoutes
 router.use(require('./partyRoutes'));
+
+//import voterRoutes
+router.use(require('./voterRoutes'));
 
 module.exports = router;
